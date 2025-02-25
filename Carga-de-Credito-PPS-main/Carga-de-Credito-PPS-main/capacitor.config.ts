77a99a0afd4ce8bbfeaa25308306f512864c8acd
MaximilianoLenzuen <60608482+MaximilianoLenzuen@users.noreply.github.com>
2025-02-25 13:57:12 -0300

    Add files via upload

---
+++ Carga-de-Credito-PPS-main/Carga-de-Credito-PPS-main/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'carga.credito',
+  appName: 'CargaDeCreditoMaximilianoLenzuen',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
